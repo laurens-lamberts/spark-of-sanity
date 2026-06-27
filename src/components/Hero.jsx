38d066c36c3be0ce2c8a1a455e7508bc1986d63e
Laurens <info@laurenslamberts.nl>
2026-06-27 20:57:11 +0200

    feat: new website feature complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,6 @@ export default function Hero() {
     <section id="top" className="hero">
       <div className="hero__overlay" />
       <div className="hero__content">
-        <img src="/assets/logo/logo-white.png" alt="Spark of Sanity logo" className="hero__logo" />
-        <h1 className="hero__title">Spark of Sanity</h1>
         <a href="#music" className="hero__cta">Listen Now</a>
       </div>
     </section>
